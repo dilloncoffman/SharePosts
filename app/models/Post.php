86b61135247efdcfd455b2a1d53437b15771fa54
Dillon Coffman <dillonscoffman@gmail.com>
2018-01-11 22:39:01 -0500

    Ability to delete posts, completing CRUD functionality. Added Welcome in navbar for current user.

--- app/models/Post.php
+++ app/models/Post.php
@@ -66,5 +66,20 @@
             return $row;
         }
 
+        public function deletePost($id){
+            $this->db->query('DELETE FROM posts WHERE id = :id');
+            // Bind id
+            $this->db->bind(':id', $id);
+
+            // Execute
+            if($this->db->execute()){
+                // Everything executed
+                return true;
+            } else {
+                // Something went wrong
+                return false;
+            }
+        }
+
 
     }
